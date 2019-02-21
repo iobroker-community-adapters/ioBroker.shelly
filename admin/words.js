bbfd5fa3f20900c4fb91393fcb05e33f54c88ab4
ldittmar81 <ldittmar.privat@gmail.com>
2019-02-21 14:08:49 +0100

    (ldittmar) automatic translation

--- admin/words.js
+++ admin/words.js
@@ -2,92 +2,12 @@
 'use strict';
 
 systemDictionary = {
-  "Auto": {
-    "en": "Auto",
-    "de": "Auto",
-    "ru": "Автоматически",
-    "pt": "Auto",
-    "nl": "Auto",
-    "fr": "Auto",
-    "it": "Auto",
-    "es": "Auto",
-    "pl": "Auto"
-  },
-  "Manual": {
-    "en": "Manual",
-    "de": "Manual",
-    "ru": "Вручную",
-    "pt": "Manual",
-    "nl": "Met de hand",
-    "fr": "Manuel",
-    "it": "Manuale",
-    "es": "Manual",
-    "pl": "Manual"
-  },
-  "on save adapter restarts with new config immediately": {
-    "en": "on save adapter restarts with new config immediately",
-    "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-    "ru": "При сохранении настроек адаптера он сразу же перезапускается",
-    "pt": "no adaptador de salvar reinicia com nova configuração imediatamente",
-    "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
-    "fr": "sur l'adaptateur de sauvegarde redémarre avec la nouvelle config immédiatement",
-    "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
-    "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato",
-    "pl": "on save adapter restarts with new config immediately"
-  },
-  "shellydescription": {
-    "en": "Shelly devices must be in the same subnet as ioBroker for working with ioBroker. ioBroker finds Shelly devices  by his own. If there are not recognized, please reboot Shelly devices once.",
-    "de": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Die Shelly Devices werden automatisch von ioBroker erkannt. Sollte das nicht der Fall sein, bitte Shelly Device rebooten.",
-    "ru": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Устройства Die Shelly были автоматизированы. Sollte das nicht der Fall sein, bitte Shelly Gerät rebooten.",
-    "pt": "Shelly Device deve ser substituído por Subnetz wie ioBroker befinden. Die Shelly Devices é um automatismo automático. Sollte das coisas do outono, bitte Shelly Gerät rebooten.",
-    "nl": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Die Shelly Devices were automatisch erkannt. Sollte das nicht der Fall sein, bitte Shelly Gerät rebooten.",
-    "fr": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Die Shelly Devices werden automatisch erkannt. Sollte dicht nicht der Fall sein, bitte Shelly Gerät rebooten.",
-    "it": "Dispositivo Shelly muss sich im gleichen Subnetz wie ioBroker befinden. Die Shelly Devices werden automatisch erkannt. Sollte das nicht der Fall sein, bitte Shelly Gerät rebooten.",
-    "es": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Die Shelly Devices werden automatisch erkannt. Sollte das nicht der Fall sein, bitte Shelly Gerät reiniciado.",
-    "pl": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Urządzenia Die Shelly werden automatisch erkannt. Sollte das nicht der Fall sein, bitte Shelly Gerät rebooten."
-  },
-  "User": {
-    "en": "User",
-    "de": "Benutzer",
-    "ru": "пользователь",
-    "pt": "Do utilizador",
-    "nl": "Gebruiker",
-    "fr": "Utilisateur",
-    "it": "Utente",
-    "es": "Usuario",
-    "pl": "Użytkownik"
-  },
-  "Password": {
-    "en": "Password",
-    "de": "Passwort",
-    "ru": "пароль",
-    "pt": "Senha",
-    "nl": "Wachtwoord",
-    "fr": "Mot de passe",
-    "it": "Parola d'ordine",
-    "es": "Contraseña",
-    "pl": "Hasło"
-  },
-  "Confirm Password": {
-    "en": "Confim Password",
-    "de": "Bestätige das Passwort",
-    "ru": "Подтвердите Пароль",
-    "pt": "Confirme a Senha",
-    "nl": "bevestig wachtwoord",
-    "fr": "Confirmez le mot de passe",
-    "it": "conferma password",
-    "es": "Confirmar contraseña",
-    "pl": "Potwierdź hasło"
-  },
-  "Password Enable": {
-    "en": "Login with password",
-    "de": "Einloggen mit Passwort",
-    "ru": "Вход с паролем",
-    "pt": "Login com senha",
-    "nl": "Inloggen met wachtwoord",
-    "fr": "Se connecter avec mot de passe",
-    "it": "Accedi con password",
-    "es": "Iniciar sesión con contraseña",
-    "pl": "Zaloguj się za pomocą hasła"
-  }
+    "Auto": {                                        "en": "Auto",                                            "de": "Auto",                                            "ru": "Автоматически"},
+    "Confirm Password": {                            "en": "Confim Password",                                 "de": "Bestätige das Passwort",                          "ru": "Подтвердите Пароль",                              "pt": "Confirme a Senha",                                "nl": "bevestig wachtwoord",                             "fr": "Confirmez le mot de passe",                       "it": "conferma password",                               "es": "Confirmar contraseña",                            "pl": "Potwierdź hasło"},
+    "Manual": {                                      "en": "Manual",                                          "de": "Manual",                                          "ru": "Вручную",                                         "pt": "Manual",                                          "nl": "Met de hand",                                     "fr": "Manuel",                                          "it": "Manuale",                                         "es": "Manual",                                          "pl": "Manual"},
+    "Password": {                                    "en": "Password",                                        "de": "Passwort",                                        "ru": "пароль",                                          "pt": "Senha",                                           "nl": "Wachtwoord",                                      "fr": "Mot de passe",                                    "it": "Parola d'ordine",                                 "es": "Contraseña",                                      "pl": "Hasło"},
+    "Password Enable": {                             "en": "Login with password",                             "de": "Einloggen mit Passwort",                          "ru": "Вход с паролем",                                  "pt": "Login com senha",                                 "nl": "Inloggen met wachtwoord",                         "fr": "Se connecter avec mot de passe",                  "it": "Accedi con password",                             "es": "Iniciar sesión con contraseña",                   "pl": "Zaloguj się za pomocą hasła"},
+    "User": {                                        "en": "User",                                            "de": "Benutzer",                                        "ru": "пользователь",                                    "pt": "Do utilizador",                                   "nl": "Gebruiker",                                       "fr": "Utilisateur",                                     "it": "Utente",                                          "es": "Usuario",                                         "pl": "Użytkownik"},
+    "on save adapter restarts with new config immediately": {"en": "on save adapter restarts with new config immediately", "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.", "ru": "При сохранении настроек адаптера он сразу же перезапускается", "pt": "no adaptador de salvar reinicia com nova configuração imediatamente", "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config", "fr": "sur l'adaptateur de sauvegarde redémarre avec la nouvelle config immédiatement", "it": "su save adapter si riavvia immediatamente con la nuova configurazione", "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato", "pl": "on save adapter restarts with new config immediately"},
+    "shellydescription": {                           "en": "Shelly devices must be in the same subnet as ioBroker for working with ioBroker. ioBroker finds Shelly devices  by his own. If there are not recognized, please reboot Shelly devices once.", "de": "Shelly Device muss sich im gleichen Subnetz wie ioBroker befinden. Die Shelly Devices werden automatisch von ioBroker erkannt. Sollte das nicht der Fall sein, bitte Shelly Device rebooten."},
 };
