5adca39022ff4b163bae9aa0dd3868fea31ef0db
Thorsten Stueben <thorsten.stueben@macbook.local>
2020-07-26 19:37:23 +0200

    coaptest

--- coaptest.js
+++ coaptest.js
@@ -1,20 +1,21 @@
-var PORT = 5683;
-var HOST; // = '127.0.0.1';
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
+let PORT = 5683;
+let HOST; //  = '127.0.0.1';
 
-server.on('listening', function () {
-    var address = server.address();
+server.on('listening', () => {
+    let address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 
     server.setMulticastLoopback(true);
     server.addMembership('224.0.1.187');
 });
 
-server.on('message', function (message, remote) {
-    console.log(new Date().toISOString() + ' - ' + remote.address + ':' + remote.port + ' - ' + message.toString('ascii'));
+server.on('message', (message, remote) => {
+    let  today = new Date();
+    console.log(today.toISOString() + ' - ' + remote.address + ':' + remote.port + ' - ' + message.toString('ascii'));
 });
-console.log(HOST);
+
 server.bind(PORT, HOST);
 
