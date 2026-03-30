bd1ff31aff852b679f8e7bf609097dfb057937b7
Martin M. <mcm57@gmx.at>
2026-03-30 20:48:11 +0200

    do not change clientId or mqtt prefix

--- lib/deviceManager.ts
+++ lib/deviceManager.ts
@@ -1,22 +1,22 @@
+import { I18n, type AdapterInstance } from '@iobroker/adapter-core';
 import {
     DeviceManagement,
-    type DeviceInfo,
-    type DeviceLoadContext,
     type ActionContext,
-    type DeviceDetails,
-    type InstanceDetails,
     type ConfigItemAny,
-    type DeviceRefresh,
     type ConfigItemState,
     type DeviceControl,
+    type DeviceDetails,
+    type DeviceInfo,
+    type DeviceLoadContext,
+    type DeviceRefresh,
+    type InstanceDetails,
 } from '@iobroker/dm-utils';
-import { type AdapterInstance, I18n } from '@iobroker/adapter-core';
 // It must be exported to index in dm-utils
-import * as dgram from 'node:dgram';
+import type { ControlState } from '@iobroker/dm-utils/build/types/base';
 import * as crypto from 'node:crypto';
+import * as dgram from 'node:dgram';
 import * as http from 'node:http';
 import * as os from 'node:os';
-import type { ControlState } from '@iobroker/dm-utils/build/types/base';
 
 class HttpAuthError extends Error {
     constructor() {
@@ -2015,10 +2015,11 @@ export default class ShellyDeviceManagement extends DeviceManagement {
             rpc_ntf: true,
             status_ntf: true,
         };
-        if (mqttId) {
-            mqttConfig.topic_prefix = mqttId;
-            mqttConfig.client_id = mqttId;
-        }
+        // client_id must be left at factory default (at least for now)
+        // if (mqttId) {
+        //     mqttConfig.topic_prefix = mqttId;
+        //     mqttConfig.client_id = mqttId;
+        // }
         const mqttResult = await this.httpPost(
             `http://${ip}/rpc/Mqtt.SetConfig`,
             JSON.stringify({ config: mqttConfig }),
