1fe0b98ab80ec6c737cc4fb599782fa0e2b7f0e0
Martin M. <mcm57@gmx.at>
2026-09-09 11:52:19 +0200

    Update objectHelper.ts

--- src/lib/objectHelper.ts
+++ src/lib/objectHelper.ts
@@ -34,11 +34,7 @@ export default class ObjectHelper {
         // delete in processObjectQueue() modifies that definition - otherwise every following call
         // would build a different object and defeat the isEquivalent() guard below, causing a
         // needless extendObject() (and thus an onObjectChange event) on every device update.
-try {
-            obj = structuredClone(obj);
-        } catch {
-            obj = JSON.parse(JSON.stringify(obj));
-        }
+        obj = JSON.parse(JSON.stringify(obj));
         (obj as ioBroker.StateObject).type ||= 'state';
         obj.common ||= {} as ioBroker.ObjectCommon;
         obj.native ||= {};
